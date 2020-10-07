060878c644117475c1a1859fd1a1f03231fea3f3
opilane <opilane@HP-CZC6297B8P>
2020-10-07 10:13:29 +0300

    bug fix

--- MyFormsDM/Form1.cs
+++ MyFormsDM/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +20,9 @@ namespace MyFormsDM
         CheckBox box_lbl, box_btn;
         RadioButton r1, r2;
         TextBox txt_box;
+        PictureBox picture;
+        TabControl tabControl;
+        TabPage page1, page2, page3;
         public Form1()
         {
             this.Height = 500;
@@ -30,7 +35,7 @@ namespace MyFormsDM
             tn.Nodes.Add(new TreeNode("Nupp - Button"));
 
 
-            Button btn = new Button();
+            btn = new Button();
             btn.Text = "Vajuta siia";
             btn.Location = new Point(50, 10);
             btn.Height = 50;
@@ -50,6 +55,9 @@ namespace MyFormsDM
             tn.Nodes.Add(new TreeNode("Märkeruut - CheckBox"));
             tn.Nodes.Add(new TreeNode("Radionupp - Radiobutton"));
             tn.Nodes.Add(new TreeNode("Tekstkast - TextBox"));
+            tn.Nodes.Add(new TreeNode("Pildicast - PictureBox"));
+            tn.Nodes.Add(new TreeNode("kaart - TabControl"));
+            tn.Nodes.Add(new TreeNode("MessageBox"));
             tree.Nodes.Add(tn);
             this.Controls.Add(tree);
             
@@ -77,6 +85,7 @@ namespace MyFormsDM
                 box_lbl.Text = "Näita Label";
                 box_lbl.Location = new Point(230, 50);
                 this.Controls.Add(box_lbl);
+                box_lbl.CheckedChanged += Box_lbl_CheckedChanged;
                 box_btn.CheckedChanged += Box_btn_CheckedChanged;
             }
             else if (e.Node.Text == "Radionupp - Radiobutton")
@@ -96,26 +105,91 @@ namespace MyFormsDM
             }
             else if (e.Node.Text == "Tekstkast - TextBox")
             {
-
+                string text;
+                try
+                {
+                    text = File.ReadAllText(path: "text.txt");
+                }
+                catch (FileNotFoundException exception)
+                {
+                    text = "tekst puudub";
+                }
                 txt_box = new TextBox();
                 txt_box.Multiline = true;
-                txt_box.Text = "";
-                txt_box.Location = new Point(300, 300);
+                txt_box.Text = text;
+                txt_box.Location = new Point(300, 200);
                 txt_box.Width = 200;
                 txt_box.Height = 100;
             }
+            else if(e.Node.Text == "Pildicast - PictureBox")
+            {
+                picture = new PictureBox();
+                picture.Image = new Bitmap("cic.png");
+                picture.Location = new Point(300, 450);
+                picture.Size = new Size (100, 100);
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+                picture.BorderStyle = BorderStyle.Fixed3D;
+                this.Controls.Add(picture);
+            }
+            else if(e.Node.Text == "kaart - TabControl")
+            {
+                tabControl = new TabControl();
+                tabControl.Location = new Point(500, 500);
+                tabControl.Size = new Size(100, 100);
+                page1 = new TabPage("Esimene");
+                page1.ImageIndex = "cic.png";
+                page2 = new TabPage("Teine");
+                page2.ImageIndex = ; 
+                page3 = new TabPage("Kolmas");
+                page3.ImageIndex = ;
+
+                tabControl.Controls.Add(page1);
+                tabControl.Controls.Add(page2);
+                tabControl.Controls.Add(page3);
+                this.Controls.Add(tabControl);
+            }
+            else if(e.Node.Text == "MessageBox")
+            {
+                MessageBox.Show("MessageBox", "Kõige lihtsam aken");
+                var answer=MessageBox.Show("Tahad inpudBoxi näha?", "Aken koos nupudega" ,MessageBoxButtons.YesNo);
+                if (answer == DialogResult.Yes)
+                {
+
+                    string text = Interaction.InputBox("Sisesta siia mingi tekst", "InputBox", "Mingi tekst");
+
+                    if (MessageBox.Show("Kas tahad tekst saada Tekskastisse?", "Teksti salvestamine", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    {
+                        lbl.Text = text;
+                        Controls.Add(lbl);
+                    }
+                }
+
+            }
         }
 
+        private void Box_lbl_CheckedChanged(object sender, EventArgs e)
+        {
+            if (box_lbl.Checked)
+            {
+                this.Controls.Add(lbl);
+            }
+            else
+            {
+                Controls.Remove(lbl);
+            }
+        }
 
         private void RadioButton_Changed(object sender, EventArgs e)
         {
             if (r1.Checked)
             {
-                btn.Location = new Point(300, 200);
+                btn.Location = new Point(10, 3);
+                lbl.Location = new Point(10, 2);
             }
             else if (r2.Checked)
             {
-                btn.Location = new Point(400, 100);
+                btn.Location = new Point(10, 100);
+                lbl.Location = new Point(10, 50);
             }
         }
 
@@ -128,14 +202,6 @@ namespace MyFormsDM
             else
             {
                 Controls.Remove(btn);
-            }
-            if(box_lbl.Checked)
-            {
-                this.Controls.Add(lbl);
-            }
-            else
-            {
-                Controls.Remove(lbl);
             }
         }
 
